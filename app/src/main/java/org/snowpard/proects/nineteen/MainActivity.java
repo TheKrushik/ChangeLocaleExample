ff78323d54a16650abd3c95790e429365529f3a5
KrusH <krushik@ukr.net>
2016-12-22 17:56:40 +0200

    reload

--- app/src/main/java/org/snowpard/proects/nineteen/MainActivity.java
+++ app/src/main/java/org/snowpard/proects/nineteen/MainActivity.java
@@ -2,108 +2,119 @@ package org.snowpard.proects.nineteen;
 
 import java.util.Locale;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.SharedPreferences;
-import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity implements OnClickListener{
+public class MainActivity extends Activity {
 
-	private TextView txt_hello;
-	private Button btn_en, btn_ru, btn_fr, btn_de;
-	private Locale myLocale;    
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		
-		this.txt_hello = (TextView)findViewById(R.id.txt_hello);
-		this.btn_en = (Button)findViewById(R.id.btn_en);
-		this.btn_ru = (Button)findViewById(R.id.btn_ru);
-		this.btn_fr = (Button)findViewById(R.id.btn_fr);
-		this.btn_de = (Button)findViewById(R.id.btn_de);
-		
-		this.btn_en.setOnClickListener(this);
-		this.btn_ru.setOnClickListener(this);
-		this.btn_fr.setOnClickListener(this);
-		this.btn_de.setOnClickListener(this);
-		
-		loadLocale();
-	}
-	
-	public void loadLocale()
-    {
-    	String langPref = "Language";
-    	SharedPreferences prefs = getSharedPreferences("CommonPrefs", Activity.MODE_PRIVATE);
-    	String language = prefs.getString(langPref, "");
-    	changeLang(language);
-    }
-    
-    public void saveLocale(String lang)
-    {
-    	String langPref = "Language";
-    	SharedPreferences prefs = getSharedPreferences("CommonPrefs", Activity.MODE_PRIVATE);
-    	SharedPreferences.Editor editor = prefs.edit();
-		editor.putString(langPref, lang);
-		editor.commit();
+    private TextView txt_hello;
+    private Locale myLocale;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        loadLocale();
+
+        TextView tvLanguage = (TextView) findViewById(R.id.tvLanguage);
+        tvLanguage.setOnClickListener(viewClickListener);
+
+        txt_hello = (TextView) findViewById(R.id.txt_hello);
     }
 
-	public void changeLang(String lang)
-    {
-    	if (lang.equalsIgnoreCase(""))
-    		return;
-    	myLocale = new Locale(lang);
-    	saveLocale(lang);
+    public void loadLocale() {
+        String langPref = "Language";
+        SharedPreferences prefs = getSharedPreferences("CommonPrefs", Activity.MODE_PRIVATE);
+        String language = prefs.getString(langPref, "");
+        changeLang(language);
+    }
+
+    public void saveLocale(String lang) {
+        String langPref = "Language";
+        SharedPreferences prefs = getSharedPreferences("CommonPrefs", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(langPref, lang);
+        editor.commit();
+    }
+
+    public void changeLang(String lang) {
+        if (lang.equalsIgnoreCase(""))
+            return;
+        myLocale = new Locale(lang);
+        saveLocale(lang);
         Locale.setDefault(myLocale);
         android.content.res.Configuration config = new android.content.res.Configuration();
         config.locale = myLocale;
         getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
-        updateTexts();
+//        updateTexts();
     }
-	
-	private void updateTexts()
-	{
-		txt_hello.setText(R.string.hello_world);
-		btn_en.setText(R.string.btn_en);
-		btn_ru.setText(R.string.btn_ru);
-		btn_fr.setText(R.string.btn_fr);
-		btn_de.setText(R.string.btn_de);
-	}
 
-	@Override
-	public void onClick(View v) {
-		String lang = "en";
-		switch (v.getId()) {
-		case R.id.btn_en:
-			lang = "en";
-			break;
-		case R.id.btn_ru:
-			lang = "ru";
-			break;
-		case R.id.btn_de:
-			lang = "de";
-			break;
-		case R.id.btn_fr:	
-			lang = "fr";
-			break;			
-		default:
-			break;
-		}
-		changeLang(lang);
-	}
-	
-	@Override
-	public void onConfigurationChanged(android.content.res.Configuration newConfig) {
-		super.onConfigurationChanged(newConfig);
-		if (myLocale != null){
-	        newConfig.locale = myLocale;
-	        Locale.setDefault(myLocale);
-	        getBaseContext().getResources().updateConfiguration(newConfig, getBaseContext().getResources().getDisplayMetrics());
-	    }
-	}
+    private void updateTexts() {
+        txt_hello.setText(R.string.hello_world);
+    }
+
+
+    @Override
+    public void onConfigurationChanged(android.content.res.Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (myLocale != null) {
+            newConfig.locale = myLocale;
+            Locale.setDefault(myLocale);
+            getBaseContext().getResources().updateConfiguration(newConfig, getBaseContext().getResources().getDisplayMetrics());
+        }
+    }
+
+    View.OnClickListener viewClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            showPopupMenu(v);
+        }
+    };
+
+    private void showPopupMenu(View v) {
+        PopupMenu popupMenu = new PopupMenu(this, v);
+        popupMenu.inflate(R.menu.menu_language);
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu1:
+                        changeLang("en");
+                        return true;
+                    case R.id.menu2:
+                        changeLang("ru");
+                        return true;
+                    case R.id.menu3:
+                        changeLang("uk");
+                        return true;
+                    default:
+                        return false;
+                }
+
+            }
+        });
+        popupMenu.setOnDismissListener(new PopupMenu.OnDismissListener() {
+            @Override
+            public void onDismiss(PopupMenu menu) {
+
+                Intent intent = getIntent();
+                overridePendingTransition(0, 0);//4
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);//5
+                finish();//6
+                overridePendingTransition(0, 0);//7
+                startActivity(intent);//8
+
+            }
+        });
+        popupMenu.show();
+    }
 }
