17bf5416d1df1f4c40d834639cf8bca934367640
KrusH <krushik@ukr.net>
2016-12-22 23:47:52 +0200

    refactor

--- app/src/main/java/org/snowpard/proects/nineteen/MainActivity.java
+++ app/src/main/java/org/snowpard/proects/nineteen/MainActivity.java
@@ -3,6 +3,7 @@ package org.snowpard.proects.nineteen;
 import java.util.Locale;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.SharedPreferences;
@@ -22,8 +23,8 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
         loadLocale();
+        setContentView(R.layout.main);
 
         TextView tvLanguage = (TextView) findViewById(R.id.tvLanguage);
         tvLanguage.setOnClickListener(viewClickListener);
@@ -52,26 +53,20 @@ public class MainActivity extends Activity {
         myLocale = new Locale(lang);
         saveLocale(lang);
         Locale.setDefault(myLocale);
-        android.content.res.Configuration config = new android.content.res.Configuration();
+        Configuration config = new Configuration();
         config.locale = myLocale;
         getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
-//        updateTexts();
     }
 
-    private void updateTexts() {
-        txt_hello.setText(R.string.hello_world);
-    }
-
-
-    @Override
-    public void onConfigurationChanged(android.content.res.Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        if (myLocale != null) {
-            newConfig.locale = myLocale;
-            Locale.setDefault(myLocale);
-            getBaseContext().getResources().updateConfiguration(newConfig, getBaseContext().getResources().getDisplayMetrics());
-        }
-    }
+//     @Override
+//    public void onConfigurationChanged(Configuration newConfig) {
+//        super.onConfigurationChanged(newConfig);
+//        if (myLocale != null) {
+//            newConfig.locale = myLocale;
+//            Locale.setDefault(myLocale);
+//            getBaseContext().getResources().updateConfiguration(newConfig, getBaseContext().getResources().getDisplayMetrics());
+//        }
+//    }
 
     View.OnClickListener viewClickListener = new View.OnClickListener() {
         @Override
@@ -80,6 +75,7 @@ public class MainActivity extends Activity {
         }
     };
 
+    //открыть языковое меню
     private void showPopupMenu(View v) {
         PopupMenu popupMenu = new PopupMenu(this, v);
         popupMenu.inflate(R.menu.menu_language);
@@ -105,16 +101,18 @@ public class MainActivity extends Activity {
         popupMenu.setOnDismissListener(new PopupMenu.OnDismissListener() {
             @Override
             public void onDismiss(PopupMenu menu) {
-
-                Intent intent = getIntent();
-                overridePendingTransition(0, 0);//4
-                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);//5
-                finish();//6
-                overridePendingTransition(0, 0);//7
-                startActivity(intent);//8
-
+                reloadApp();
             }
         });
         popupMenu.show();
+    }
+//перезагрузка приложения
+    private void reloadApp() {
+        Intent intent = getIntent();
+        overridePendingTransition(0, 0);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        finish();
+        overridePendingTransition(0, 0);
+        startActivity(intent);
     }
 }
